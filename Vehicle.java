adfe7e5d0a1ec551e5eaf7ba333759eb425311bd
Kalle <kalle@kakmonstret.se>
2026-01-28 11:26:27 +0100

    refactor: rearrange code

--- Vehicle.java
+++ Vehicle.java
@@ -1,9 +1,9 @@
 import java.awt.*;
 
 abstract class Vehicle extends MovableObject {
+    private final String modelName;
     private double currentSpeed;
     private Color color;
-    private final String modelName;
 
     public Vehicle(double currentSpeed, Color color, String modelName) {
         super(0, 0, 0);  // start position (0, 0)
@@ -17,15 +17,15 @@ abstract class Vehicle extends MovableObject {
         return currentSpeed;
     }
 
-    public void setColor(Color clr) {
-        color = clr;
+    protected void setCurrentSpeed(double speed) {
+        this.currentSpeed = Math.max(speed, 0);
     }
 
     public Color getColor() {
         return color;
     }
 
-    protected void setCurrentSpeed(double speed) {
-        this.currentSpeed = Math.max(speed, 0);
+    public void setColor(Color clr) {
+        color = clr;
     }
 }
