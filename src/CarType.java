136d81fef80b9568efcbe9fbda0396ad163f51ec
Oliver Isaksson <oliverisaksson@proton.me>
2026-03-05 14:38:55 +0100

    fix: move images into constants

--- src/CarType.java
+++ src/CarType.java
@@ -1,7 +1,17 @@
 package src;
 
 public enum CarType {
-    VOLVO240,
-    SAAB95,
-    SCANIA
+    VOLVO240("pics/Volvo240.jpg"),
+    SAAB95("pics/Saab95.jpg"),
+    SCANIA("pics/Scania.jpg");
+
+    CarType(String imgPath) {
+        this.imgPath = imgPath;
+    }
+
+    public String getImgPath() {
+        return imgPath;
+    }
+
+    private final String imgPath;
 }
